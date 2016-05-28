b31350756d0627f2251a8ce1b9031a961fc0ffb3
panweiyan <812277362@qq.com>
2016-05-28 22:40:17 +0800

    update code

--- js/script.js
+++ js/script.js
@@ -1,122 +1,132 @@
+var ScreenSaver = function(){
+    this.colors = ["#87D370","#F7E062","#D0DB54","#DAD361","#F38636"];
+    this.canvas = document.querySelector('canvas');
+    this.cxt=canvas.getContext("2d");
+    this.balls = new Array();
+    this.window_width = document.documentElement.clientWidth;
+    this.window_height = document.documentElement.clientHeight;
+    this.r = 80;
+    this.Num = 8;
+    this.n = 0;
+    this.timer = null;
+    this.init();
+}
 
-        var colors = ["#87D370","#F7E062","#D0DB54","#DAD361","#F38636"];
-		var canvas = document.querySelector('canvas');
-		var cxt=canvas.getContext("2d");
-		var balls = new Array();
-		var window_width = document.documentElement.clientWidth;
-        var window_height = document.documentElement.clientHeight;
-        canvas.width = window_width;
-        canvas.height = window_height;
-
-        var wall = {
-            r:90,
-            Num:8,
-            n:0,
-            timer:null,
-            s: document.getElementById('canvas'),
-            createBall:function(){
-                var that = this;
-                var aball={
+ScreenSaver.prototype = {
+    init:function(){
+        this.initCanvas();
+        this.screenHide();
+        var that = this;
+        document.onmousemove = function(){
+            that.event();
+        };
+        document.onkeydown = function(){
+            that.event();
+        };
+    },
+    initCanvas:function(){
+        this.canvas.width = this.window_width;
+        this.canvas.height = this.window_height;
+    },
+    createBall:function(){
+        var that = this;
+        var aball={
                     x:0,
-                    y:window_height,
+                    y:that.window_height,
                     r:that.r,
                     vX:that.getRandom(0.8, 1.2),
                     vY:that.getRandom(1.2, 1.5),
-                    color:colors[that.getRandom(0, colors.length-1)]
-                }
-                if(++(that.n) <= that.Num){
-                    balls.push(aball);
-                    setTimeout(function(){
-                        that.createBall();
-                    }, 1000);
-                }
-            },
-            drawBall:function(){
+                    color:that.colors[that.getRandom(0, that.colors.length-1)]
+        }
+        if(++(that.n) <= that.Num){
+            that.balls.push(aball);
+            setTimeout(function(){
+                that.createBall();
+            }, 1000);
+        }
+    },
+    drawBall:function(){
                 var that = this;
-                cxt.clearRect(0,0,window_width,window_height);
-                cxt.fillStyle = "#6F964E";
-                cxt.fillRect(0,0,window_width,window_height);
-                for (var i=0;i<balls.length;i++){
-                    balls[i].x += balls[i].vX;
-                    balls[i].y += balls[i].vY;
+                this.cxt.clearRect(0,0,this.canvas.width,this.canvas.height);
+                this.cxt.fillStyle = "#6F964E";
+                this.cxt.fillRect(0,0,this.canvas.width,this.canvas.height);
+                for (var i=0,len=this.balls.length;i<len;i++){
+                    this.balls[i].x += this.balls[i].vX;
+                    this.balls[i].y += this.balls[i].vY;
                                         
-                    if(balls[i].x >= window_width-that.r){
-                        balls[i].x = window_width-that.r;
-                        balls[i].vX *= -1;
-                    }
-                    if(balls[i].x <= that.r){
-                        balls[i].x = that.r;
-                        balls[i].vX *= -1;
-                    }           
-                    if(balls[i].y >= canvas.height-that.r){
-                        balls[i].y = canvas.height-that.r;
-                        balls[i].vY *= -1;
-                    }
-                    if(balls[i].y <= that.r){
-                        balls[i].y = that.r;
-                        balls[i].vY *= -1;
-                    }
+                    if(this.balls[i].x >= this.window_width-this.r){
+                        this.balls[i].x = this.window_width-that.r;
+                        this.balls[i].vX *= -1;
+                    };
+                    if(this.balls[i].x <= this.r){
+                       this.balls[i].x = this.r;
+                        this.balls[i].vX *= -1;
+                    };         
+                    if(this.balls[i].y >= this.window_height-this.r){
+                        this.balls[i].y = this.window_height-this.r;
+                        this.balls[i].vY *= -1;
+                    };
+                    if(this.balls[i].y <= this.r){
+                        this.balls[i].y = this.r;
+                        this.balls[i].vY *= -1;
+                    };
 
-                    for(var j=0; j<balls.length; j++){
+                    for(var j=0; j<this.balls.length; j++){
                         if(j!==i){
-                            var bx = balls[i].x-balls[j].x;
-                            var by = balls[i].y-balls[j].y;
+                            var bx = this.balls[i].x-this.balls[j].x;
+                            var by = this.balls[i].y-this.balls[j].y;
                             var dist = Math.sqrt(bx*bx + by*by);
-                            if( dist < (that.r*2)) {
-                                var tempX=balls[i].vX;
-                                var tempY=balls[i].vY;
-                                balls[i].vX=balls[j].vX;
-                                balls[j].vX=tempX;
-                                balls[i].vY=balls[j].vY;
-                                balls[j].vY=tempY;
+                            if( dist <= (this.r*2)) {
+                                var tempX=this.balls[i].vX;
+                                var tempY=this.balls[i].vY;
+                                this.balls[i].vX=this.balls[j].vX;
+                                this.balls[j].vX=tempX;
+                                this.balls[i].vY=this.balls[j].vY;
+                                this.balls[j].vY=tempY;
                             }
                         }
                     }
-                    cxt.beginPath();
-                    cxt.arc(balls[i].x,balls[i].y,balls[i].r,0,Math.PI*2,true);
-                    cxt.closePath();
+                    this.cxt.beginPath();
+                    this.cxt.arc(this.balls[i].x,this.balls[i].y,this.balls[i].r,0,Math.PI*2,true);
+                    this.cxt.closePath();
 
-                    var g = cxt.createRadialGradient(balls[i].x-70,balls[i].y-36,0,balls[i].x,balls[i].y,that.r+10);
+                    var g = this.cxt.createRadialGradient(this.balls[i].x-70,this.balls[i].y-36,0,this.balls[i].x,this.balls[i].y,this.r+10);
                     g.addColorStop(0,"transparent"); 
-                    g.addColorStop(1,balls[i].color);
-                    cxt.fillStyle = g;
+                    g.addColorStop(1,this.balls[i].color);
+                    this.cxt.fillStyle = g;
 
-                    cxt.globalAlpha = 0.6;
-                    cxt.fill(); 
-                    cxt.strokeStyle = balls[i].color;
-                    cxt.lineWidth = 1;
-                    cxt.stroke();
-                }
+                    this.cxt.globalAlpha = 0.6;
+                    this.cxt.fill(); 
+                    this.cxt.strokeStyle = this.balls[i].color;
+                    this.cxt.lineWidth = 1;
+                    this.cxt.stroke();
+                };
                 setTimeout(function(){
                     that.drawBall();
                 },10);
             },
-            screen:function(){
-                var that = this;
-                that.s.style.display = 'block';
+            screenShow:function(){
+                this.canvas.style.display = 'block';
             },
-            hide:function(){
-                var that = this;
-                that.s.style.display = 'none';
+            screenHide:function(){
+                this.canvas.style.display = 'none';
             }, 
             getRandom:function(min, max){
                 return (Math.floor(Math.random() * (max - min + 1)) + min);
             },
-        };
+            event:function(){
+                var that = this;
+                this.screenHide();
+                this.balls.length = 0;
+                this.n = 0;
+                clearTimeout(this.timer);
+                this.timer = setTimeout(function(){
+                    that.createBall();
+                    that.drawBall();
+                    that.screenShow();
+                },3000);
+            },
+};
 
-        function event(){
-                wall.hide();
-                balls.length = 0;
-                wall.n = 0;
-                clearTimeout(wall.timer);
-                wall.timer = setTimeout(function(){
-                    wall.createBall();
-                    wall.drawBall();
-                    wall.screen();
-                },3000)
-        }
-        document.onmousemove  = function(){event()};
-        document.onkeydown = function(){event()};
-
-        wall.hide();
+var screenSaver = new ScreenSaver();
 
